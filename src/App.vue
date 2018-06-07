c1030198d4cc315c723fda26e2827b54cbe1f14f
y.decoux <y.decoux@gmail.com>
2018-06-08 00:53:12 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <PlayerList/>
+    <HelloWorld msg="Hey, Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import PlayerList from './components/PlayerList.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    PlayerList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PlayerList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div v-for="player in players" v-bind:key="player.id">
+            {{player.name}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'PlayerList',
+    data: function () {
+        return {
+            players:[{
+            id:'123',
+            name: 'Foo',
+            score: 45
+        },{
+            id:'456',
+            name: 'Bar',
+            score: 14
+        },{
+            id:'789',
+            name: 'Baz',
+            score: 43
+        },
+        ]}
+    }
+}
+</script>
+
+<style>
+
+</style>
